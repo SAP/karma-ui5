c0dfab709e4e9cd1caabd7f3942b11a80c7b878d
Matthias Osswald <mat.osswald@sap.com>
2017-11-07 09:42:27 +0100

    Add autorun feature
    Test modules can be defined in the openui5 config and will be loaded
    automatically via sap.ui.require.
    So a custom "karma-main.js" is not require anymore, but can still be
    used to manually load and run tests.

---
+++ src/autorun.js
@@ -0,0 +1,20 @@
+var karma = window.__karma__;
+var config = karma.config;
+var ui5config = (config && config.openui5) || {};
+var ui5configTests = ui5config.tests;
+
+if (!ui5configTests) {
+	// No tests defined - skipping autorun...
+	// Tests must be loaded manually
+	return;
+}
+
+// Prevent Karma from running prematurely.
+karma.loaded = function () {};
+
+sap.ui.getCore().attachInit(function() {
+	sap.ui.require(ui5configTests, function() {
+		// Finally, start Karma to run the tests.
+		karma.start();
+	});
+});
