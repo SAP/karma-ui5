7cc93d0d6073fa33980eeab7f59ed4f2e66ddfa1
Roman Rommel <roman.rommel@sap.com>
2019-03-20 10:06:54 +0100

    [INTERNAL] Run tests in iFrame by default

--- test/integration/karma-base.conf.js
+++ test/integration/karma-base.conf.js
@@ -3,12 +3,6 @@ process.env.CHROME_BIN = require("puppeteer").executablePath();
 module.exports = function(config) {
 	config.set({
 
-		client: {
-			ui5: {
-				useIframe: true
-			}
-		},
-
 		customLaunchers: {
 			ChromeHeadlessNoSandbox: {
 				base: "ChromeHeadless",
