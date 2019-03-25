9b76627a82e1d6a617f891d532a95083bcaa0dfe
Matthias Osswald <mat.osswald@sap.com>
2019-03-25 16:21:26 +0100

    [FIX] Don't create webpack bundle on postinstall
    This reduces the list of dependencies required to be installed by
    consumers of this plugin and makes sure to create the bundle before
    publishing instead.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 const LicenseWebpackPlugin = require("license-webpack-plugin").LicenseWebpackPlugin;
+const allowedLicenses = [
+	"MIT",
+	"BSD-3-Clause"
+];
 
 module.exports = {
 	mode: "production",
@@ -11,6 +15,24 @@ module.exports = {
 		filename: "browser-bundle.js"
 	},
 	plugins: [
-		new LicenseWebpackPlugin()
+		new LicenseWebpackPlugin({
+			addBanner: true,
+			unacceptableLicenseTest: (licenseType) => {
+				// Fail when not allowed licenses would be packaged
+				return !allowedLicenses.includes(licenseType);
+			},
+			renderLicenses: (modules) => {
+				const line = (v) => v ? `\n${v}` : "";
+				return modules
+					.sort((a, b) => a.name < b.name ? -1 : 1)
+					.map((module) => {
+						let text = `${module.name} (${module.packageJson.version})`;
+						text += line(module.packageJson.homepage);
+						text += line(module.licenseId);
+						text += line(module.licenseText);
+						return text;
+					}).join("\n\n");
+			}
+		})
 	]
 };
