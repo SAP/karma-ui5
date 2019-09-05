55a159d626fc438ef076862bc108a3f01deacc7b
Matthias Osswald <mat.osswald@sap.com>
2019-09-05 13:44:52 +0200

    Sort urlParameters / fix package-lock

--- test/integration/application-ui5-tooling-url-parameters/webapp/test/test.qunit.js
+++ test/integration/application-ui5-tooling-url-parameters/webapp/test/test.qunit.js
@@ -6,7 +6,7 @@ sap.ui.getCore().attachInit(function() {
 	"use strict";
 
 	QUnit.test("URL Parameters", function(assert) {
-		assert.strictEqual(document.location.search, encodeURI("?hidepassed=true&0=0️⃣&0="),
+		assert.strictEqual(document.location.search, encodeURI("?0=0️⃣&0=&hidepassed=true"),
 			"Configured URL parameters got applied");
 		assert.ok(QUnit.config.hidepassed, "URL parameter configured QUnit");
 	});
