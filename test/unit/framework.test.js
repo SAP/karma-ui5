SAP/karma-ui5

commit 20b651258722973ec8dc4a8391808dab8bc335f0

[FIX] Improve performance when serving UI5 resources via URL
Using an agent with "keepAlive" improves performance when proxing
requests, especially on Windows.

lib/framework.js
@@ -2,6 +2,7 @@ const normalizer = require("@ui5/project").normalizer;
 const ui5Fs = require("@ui5/fs");
 const resourceFactory = ui5Fs.resourceFactory;
 const ReaderCollectionPrioritized = ui5Fs.ReaderCollectionPrioritized;
+const http = require("http");
 const httpProxy = require("http-proxy");
 const fs = require("fs");
 const path = require("path");
@@ -440,9 +441,11 @@ class Framework {
 	}
 
 	setupProxy({url}) {
+		const agent = new http.Agent({keepAlive: true});
 		const proxy = httpProxy.createProxyServer({
 			target: url,
-			changeOrigin: true
+			changeOrigin: true,
+			agent
 		});
 
 		return {

test/unit/framework.test.js
@@ -147,9 +147,13 @@ describe("Proxy for UI5 ", () => {
 
 		const createProxyServer = require("http-proxy").createProxyServer;
 
-		expect(createProxyServer).toBeCalledWith({
+		const lastCall = createProxyServer.mock.calls[createProxyServer.mock.calls.length - 1];
+		expect(lastCall[0]).toMatchObject({
 			target: "http://localhost",
-			changeOrigin: true
+			changeOrigin: true,
+			agent: expect.objectContaining({
+				keepAlive: true
+			})
 		});
 
 		// const proxy = require("http-proxy").createProxyServer.mock.results[0].value;

commit 018730eb918e3f1cd9f5068906fa6789d6be300f

[FIX] Protocol error when using https ui5.url
Regression caused by https://github.com/SAP/karma-ui5/pull/187

Fixes: https://github.com/SAP/karma-ui5/issues/188

lib/framework.js
@@ -2,7 +2,9 @@ const normalizer = require("@ui5/project").normalizer;
 const ui5Fs = require("@ui5/fs");
 const resourceFactory = ui5Fs.resourceFactory;
 const ReaderCollectionPrioritized = ui5Fs.ReaderCollectionPrioritized;
+const {parse: parseUrl} = require("url");
 const http = require("http");
+const https = require("https");
 const httpProxy = require("http-proxy");
 const fs = require("fs");
 const path = require("path");
@@ -441,7 +443,9 @@ class Framework {
 	}
 
 	setupProxy({url}) {
-		const agent = new http.Agent({keepAlive: true});
+		const {protocol} = parseUrl(url);
+		const Agent = protocol === "https:" ? https.Agent : http.Agent;
+		const agent = new Agent({keepAlive: true});
 		const proxy = httpProxy.createProxyServer({
 			target: url,
 			changeOrigin: true,

test/unit/framework.test.js
@@ -140,7 +140,7 @@ describe("Middleware for UI5", () => {
 });
 
 describe("Proxy for UI5 ", () => {
-	it("Should call proxy module from serveResources middleware", (done) => {
+	it("Should call proxy module from serveResources middleware (http)", (done) => {
 		const proxyServer = new Framework().setupProxy({
 			url: "http://localhost"
 		});
@@ -152,7 +152,38 @@ describe("Proxy for UI5 ", () => {
 			target: "http://localhost",
 			changeOrigin: true,
 			agent: expect.objectContaining({
-				keepAlive: true
+				keepAlive: true,
+				protocol: "http:"
+			})
+		});
+
+		// const proxy = require("http-proxy").createProxyServer.mock.results[0].value;
+
+		expect(proxyServer.serveThemes).toBeUndefined();
+
+		const req = {};
+		const res = {};
+		const next = function() {
+			// expect(proxy.web).toBeCalledWith(req, res, next); // TODO: check why this fails
+			done();
+		};
+		proxyServer.serveResources(req, res, next);
+	});
+
+	it("Should call proxy module from serveResources middleware (https)", (done) => {
+		const proxyServer = new Framework().setupProxy({
+			url: "https://localhost"
+		});
+
+		const createProxyServer = require("http-proxy").createProxyServer;
+
+		const lastCall = createProxyServer.mock.calls[createProxyServer.mock.calls.length - 1];
+		expect(lastCall[0]).toMatchObject({
+			target: "https://localhost",
+			changeOrigin: true,
+			agent: expect.objectContaining({
+				keepAlive: true,
+				protocol: "https:"
 			})
 		});
 