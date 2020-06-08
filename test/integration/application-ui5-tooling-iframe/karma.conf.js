7e40986d1e9cf1f2d10c8cc8da448b5555c79118
Tobias Sorn <tobias.sorn@sap.com>
2020-06-08 16:31:23 +0200

    further test
    set karma version back to a stable version
    with regards to the exitCode

---
+++ test/integration/application-ui5-tooling-iframe/karma.conf.js
@@ -0,0 +1,46 @@
+module.exports = function(config) {
+	"use strict";
+
+	require("../karma-base.conf")(config);
+	require("../../../")();
+	config.set({
+
+		frameworks: ["ui5"],
+
+		preprocessors: {
+			"{webapp,webapp/!(test)}/*.js": ["coverage"]
+		},
+
+		coverageReporter: {
+
+			includeAllSources: true,
+			reporters: [
+				{
+					type: "json",
+					dir: "coverage",
+					subdir: "json"
+				}
+			],
+			check: {
+				each: {
+					statements: 100,
+					branches: 100,
+					functions: 100,
+					lines: 100
+				}
+			}
+		},
+
+		reporters: ["progress", "coverage"],
+
+		logLevel: "DEBUG"
+
+	});
+};
+
+module.exports.assertions = function({expect, log}) {
+	const coverage = require("./coverage/json/coverage-final.json");
+	const files = Object.keys(coverage);
+	expect(files).toHaveLength(1);
+	expect(files[0]).toEndWith("application-ui5-tooling-iframe/webapp/foo.js");
+};
