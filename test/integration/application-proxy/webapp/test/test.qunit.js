e494d2dfb0e930382f5de32f59ba5e4a56b8cee3
Matthias Osswald <mat.osswald@sap.com>
2020-08-19 13:21:05 +0200

    [FIX] HTML mode: QUnit.config.noglobals with coverage enabled
    Code instrumented by istanbul introduces global variables
    (cov_* for each file and a global __coverage__ variable).
    This causes a failure when the "noglobals" QUnit config is active and
    some file is loaded during a test as this pollutes the global namespace.
    
    Hooking into the "after" function allows to add those variables to the
    list of known globals so that they won't be detected as new globals.

--- test/integration/application-proxy/webapp/test/test.qunit.js
+++ test/integration/application-proxy/webapp/test/test.qunit.js
@@ -5,11 +5,24 @@ QUnit.config.autostart = false;
 sap.ui.getCore().attachInit(function() {
 	"use strict";
 
-	sap.ui.require(["test/app/foo"], function() {
-		QUnit.test("Karma", function(assert) {
-			assert.ok(parent.__karma__.files["/base/webapp/.dotfile"], "Karma files should contain dotfiles");
-		});
-
-		QUnit.start();
+	QUnit.test("Karma", function(assert) {
+		assert.ok(parent.__karma__.files["/base/webapp/.dotfile"], "Karma files should contain dotfiles");
 	});
+
+	QUnit.test(
+		"Loading files during a test should not result into 'Introduced global variable(s)' issues " +
+		"when QUnit.config.noglobals is active",
+		function(assert) {
+			const done = assert.async();
+			sap.ui.require(["test/app/foo"], function() {
+				assert.ok(true, "test/app/foo has been loaded");
+				done();
+			}, function(err) {
+				assert.ok(false, "Failed to load test/app/foo: " + err);
+			});
+		}
+	);
+	QUnit.config.noglobals = true;
+
+	QUnit.start();
 });
