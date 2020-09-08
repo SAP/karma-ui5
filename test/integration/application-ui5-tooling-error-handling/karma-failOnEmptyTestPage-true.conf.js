645dc98511ff026ac01afdffe1d4b2764d892dce
Matthias Osswald <mat.osswald@sap.com>
2020-09-08 13:44:08 +0200

    Fix assertion pattern

--- test/integration/application-ui5-tooling-error-handling/karma-failOnEmptyTestPage-true.conf.js
+++ test/integration/application-ui5-tooling-error-handling/karma-failOnEmptyTestPage-true.conf.js
@@ -19,6 +19,6 @@ module.exports = function(config) {
 module.exports.shouldFail = true;
 module.exports.assertions = ({expect, log}) => {
 	expect(log).toMatch(/Executed 2 of 2/);
-	expect(log).toMatch(/\/base\/webapp\/test\/empty-testpage\/empty.qunit.html FAILED/);
+	expect(log).toMatch(/\/base\/webapp\/test\/empty-testpage\/empty\.qunit\.html FAILED/);
 	expect(log).toMatch(/Testpage did not define any tests./);
 };
