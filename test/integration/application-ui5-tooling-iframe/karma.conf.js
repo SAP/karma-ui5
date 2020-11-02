775545ded8e5eb50fa5c4ff37cebef582a17a0c1
2499128545@qq.com <2499128545@qq.com>
2020-11-02 15:27:03 +0800

    fix ui5 tooling iframe path assertion fail on windows

--- test/integration/application-ui5-tooling-iframe/karma.conf.js
+++ test/integration/application-ui5-tooling-iframe/karma.conf.js
@@ -40,7 +40,10 @@ module.exports = function(config) {
 
 module.exports.assertions = function({expect, log}) {
 	const coverage = require("./coverage/json/coverage-final.json");
+	const sWindowsExpect = "application-ui5-tooling-iframe\\webapp\\foo.js";
+	const sLinuxExpect = "application-ui5-tooling-iframe/webapp/foo.js";
 	const files = Object.keys(coverage);
 	expect(files).toHaveLength(1);
-	expect(files[0]).toEndWith("application-ui5-tooling-iframe/webapp/foo.js");
+	const sActual = files[0] && files[0].replace(sWindowsExpect, sLinuxExpect);
+	expect(sActual).toEndWith(sLinuxExpect);
 };
