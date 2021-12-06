603a4fad8a36aaddd7b5a62df6f56d322e3ab1f3
Lars Kissel <lars.kissel@sap.com>
2021-12-06 15:24:49 +0100

    Improve error handling

--- test/unit/fileExportReporter.test.js
+++ test/unit/fileExportReporter.test.js
@@ -6,10 +6,23 @@ const mkdirp = require("mkdirp");
 const FileExportReporter = require("../../lib/fileExportReporter");
 
 describe("fileExportReporter plugin", () => {
+	let log;
+	let logger;
 	let fsAccessMock;
 	let fsWriteFileMock;
 	let pathJoinMock;
 	beforeEach(() => {
+		log = {
+			info: jest.fn(),
+			warn: jest.fn(),
+			debug: jest.fn()
+		};
+		logger = {
+			create: jest.fn(() => {
+				return log;
+			})
+		};
+
 		fsAccessMock = jest.spyOn(fs, "access");
 		fsWriteFileMock = jest.spyOn(fs, "writeFile");
 		pathJoinMock = jest.spyOn(path, "join");
@@ -21,9 +34,10 @@ describe("fileExportReporter plugin", () => {
 	});
 
 	const base = jest.fn();
-	const resolvedTestPath = "x://some/path/myFileExportDir";
+	const baseDir = "x://some/path/myFileExportDir";
+	const resolvedTestPath = "/resolved/path";
 	const config = {
-		basePath: resolvedTestPath,
+		basePath: baseDir,
 		ui5: {
 			fileExport: {
 				outputDir: "myFileExportDir"
@@ -31,20 +45,10 @@ describe("fileExportReporter plugin", () => {
 		}
 	};
 	const configNoDir = {
-		basePath: resolvedTestPath,
+		basePath: baseDir,
 		ui5: {
 			fileExport: true
 		}
-	};
-	const log = {
-		info: jest.fn(),
-		warn: jest.fn(),
-		debug: jest.fn()
-	};
-	const logger = {
-		create: jest.fn(() => {
-			return log;
-		})
 	};
 
 	it("Reading outputDir from config", async () => {
@@ -59,8 +63,8 @@ describe("fileExportReporter plugin", () => {
 		expect(base).toBeCalledWith(fileExportReporterDefaultPath);
 
 		expect(pathJoinMock).toHaveBeenCalledTimes(2);
-		expect(pathJoinMock).toBeCalledWith(resolvedTestPath, "./karma-ui5-reports");
-		expect(pathJoinMock).toBeCalledWith(resolvedTestPath, "myFileExportDir");
+		expect(pathJoinMock).toBeCalledWith(baseDir, "./karma-ui5-reports");
+		expect(pathJoinMock).toBeCalledWith(baseDir, "myFileExportDir");
 
 		expect(log.debug).toHaveBeenCalledTimes(2);
 		expect(log.debug).toBeCalledWith("outputDir is: " + resolvedTestPath + "/path1");
@@ -79,6 +83,39 @@ describe("fileExportReporter plugin", () => {
 		expect(log.debug).toBeCalledWith("No export files provided");
 	});
 
+	it("onBrowserComplete - exportFiles must be an array", async () => {
+		const browser = {};
+		const result = {
+			exportFiles: "foo"
+		};
+
+		const fileExportReporter = new FileExportReporter(base, config, logger);
+		fileExportReporter.onBrowserComplete(browser, result);
+
+		expect(log.warn).toHaveBeenCalledTimes(1);
+		expect(log.warn).toBeCalledWith("Export files must be given as an array");
+	});
+
+	it("onBrowserComplete - export file's name and content must be of type string", async () => {
+		const browser = {};
+		const result = {
+			exportFiles: [{
+				name: {},
+				content: "foo"
+			}, {
+				name: "foo",
+				content: {}
+			}]
+		};
+
+		const fileExportReporter = new FileExportReporter(base, config, logger);
+		fileExportReporter.onBrowserComplete(browser, result);
+
+		expect(log.warn).toHaveBeenCalledTimes(2);
+		expect(log.warn).toBeCalledWith("Invalid file object. \"name\" and \"content\" must be strings");
+		expect(log.warn).toBeCalledWith("Invalid file object. \"name\" and \"content\" must be strings");
+	});
+
 	it("onBrowserComplete - tests crashed", async () => {
 		const browser = {};
 
@@ -90,22 +127,24 @@ describe("fileExportReporter plugin", () => {
 
 	it("onBrowserComplete - save incoming export files", async () => {
 		const browser = {};
-		const filePath1 = "./karma-ui5-reports/filePath1";
-		const filePath2 = "./karma-ui5-reports/filePath2";
+		const filePath1 = resolvedTestPath + "/filePath1";
+		const filePath2 = resolvedTestPath + "/filePath2";
 		const result = {
 			exportFiles: [{
 				name: "filename1",
 				content: "content1"
 			}, {
-				name: "/some/path/filename2", // prevent using deeper path structures
+				name: "\\some:*?/path<>|/filename2", // test escapeFileName
 				content: "content2"
 			}]
 		};
 
 		pathJoinMock
-			.mockReturnValueOnce(resolvedTestPath)
-			.mockReturnValueOnce(filePath1)
-			.mockReturnValueOnce(filePath2);
+			.mockReturnValueOnce(resolvedTestPath) // outputDir
+			.mockReturnValueOnce(filePath1) // getUniqueFileName
+			.mockReturnValueOnce(filePath1) // pathToWrite
+			.mockReturnValueOnce(filePath2) // getUniqueFileName
+			.mockReturnValueOnce(filePath2); // pathToWrite
 		fsAccessMock
 			.mockRejectedValueOnce({code: "ENOENT"})
 			.mockRejectedValueOnce({code: "ENOENT"});
@@ -116,22 +155,24 @@ describe("fileExportReporter plugin", () => {
 		const fileExportReporter = new FileExportReporter(base, config, logger);
 		await fileExportReporter.onBrowserComplete(browser, result);
 
+		expect(pathJoinMock).toBeCalledWith(resolvedTestPath, ".some.path.filename2"); // test escapeFileName
+
 		expect(mkdirp).toHaveBeenCalledTimes(2);
-		expect(mkdirp).toBeCalledWith("./karma-ui5-reports");
+		expect(mkdirp).toBeCalledWith(resolvedTestPath);
 
 		expect(fsWriteFileMock).toHaveBeenCalledTimes(2);
 		expect(fsWriteFileMock).toBeCalledWith(filePath1, "content1");
 		expect(fsWriteFileMock).toBeCalledWith(filePath2, "content2");
 
-		expect(log.info).toBeCalledWith("Writing file: " + filePath1);
+		expect(log.debug).toBeCalledWith("Writing file: " + filePath1);
 		expect(log.info).toBeCalledWith("Saved file '" + filePath1 + "'");
-		expect(log.info).toBeCalledWith("Writing file: " + filePath2);
-		expect(log.warn).toBeCalledWith("Failed to write file\n\terrorMsg");
+		expect(log.debug).toBeCalledWith("Writing file: " + filePath2);
+		expect(log.warn).toBeCalledWith("Failed to write file " + filePath2 + "\n\terrorMsg");
 	});
 
 	it("onBrowserComplete - save incoming export files (different browsers)", async () => {
 		const browser1 = {name: "BrowserA"};
-		const browser2 = {name: "/some/path/BrowserB"}; // prevent using deeper path structures
+		const browser2 = {name: "some:*/path<>/BrowserB"}; // test escapeFileName
 		const result1 = {
 			exportFiles: [{
 				name: "filename1",
@@ -146,9 +187,13 @@ describe("fileExportReporter plugin", () => {
 		};
 
 		pathJoinMock
-			.mockReturnValueOnce(resolvedTestPath)
-			.mockReturnValueOnce("/BrowserA/filename1")
-			.mockReturnValueOnce("/BrowserB/filename2");
+			.mockReturnValueOnce(resolvedTestPath) // outputDir
+			.mockReturnValueOnce(resolvedTestPath + "/BrowserA") // outputDir + browser
+			.mockReturnValueOnce(resolvedTestPath + "/BrowserA/filename1") // getUniqueFileName
+			.mockReturnValueOnce(resolvedTestPath + "/BrowserA/filename1") // pathToWrite
+			.mockReturnValueOnce(resolvedTestPath + "/some.path.BrowserB") // outputDir + browser
+			.mockReturnValueOnce(resolvedTestPath + "/some.path.BrowserB/filename2") // getUniqueFileName
+			.mockReturnValueOnce(resolvedTestPath + "/some.path.BrowserB/filename2"); // pathToWrite
 		fsAccessMock
 			.mockRejectedValueOnce({code: "ENOENT"})
 			.mockRejectedValueOnce({code: "ENOENT"});
@@ -161,19 +206,20 @@ describe("fileExportReporter plugin", () => {
 		await fileExportReporter.onBrowserComplete(browser1, result1);
 		await fileExportReporter.onBrowserComplete(browser2, result2);
 
-		expect(pathJoinMock).toHaveBeenCalledTimes(3);
-		expect(pathJoinMock).toBeCalledWith(resolvedTestPath, "myFileExportDir"); // outputDir creation
-		expect(pathJoinMock).toBeCalledWith(resolvedTestPath, "BrowserA", "filename1");
-		expect(pathJoinMock).toBeCalledWith(resolvedTestPath, "BrowserB", "filename2");
+		expect(pathJoinMock).toHaveBeenCalledTimes(7);
+		expect(pathJoinMock).toBeCalledWith(baseDir, "myFileExportDir"); // outputDir
+		expect(pathJoinMock).toBeCalledWith(resolvedTestPath, "BrowserA"); // outputDir + browser
+		expect(pathJoinMock).toBeCalledWith(resolvedTestPath + "/BrowserA", "filename1");
+		expect(pathJoinMock).toBeCalledWith(resolvedTestPath, "some.path.BrowserB"); // outputDir + browser
+		expect(pathJoinMock).toBeCalledWith(resolvedTestPath + "/some.path.BrowserB", "filename2");
 
 		expect(fsWriteFileMock).toHaveBeenCalledTimes(2);
-		expect(fsWriteFileMock).toBeCalledWith("/BrowserA/filename1", "content1");
-		expect(fsWriteFileMock).toBeCalledWith("/BrowserB/filename2", "content2");
+		expect(fsWriteFileMock).toBeCalledWith(resolvedTestPath + "/BrowserA/filename1", "content1");
+		expect(fsWriteFileMock).toBeCalledWith(resolvedTestPath + "/some.path.BrowserB/filename2", "content2");
 	});
 
 	it("onBrowserComplete - save incoming export files (ensure unique file names)", async () => {
 		const browser = {};
-		const filePath = "./karma-ui5-reports/filename";
 		const result = {
 			exportFiles: [{
 				name: "filename",
@@ -187,11 +233,6 @@ describe("fileExportReporter plugin", () => {
 			}]
 		};
 
-		pathJoinMock
-			.mockReturnValueOnce(resolvedTestPath)
-			.mockReturnValueOnce(filePath)
-			.mockReturnValueOnce(filePath)
-			.mockReturnValueOnce(filePath);
 		fsAccessMock
 			// file 1
 			.mockRejectedValueOnce({code: "ENOENT"})
@@ -211,8 +252,83 @@ describe("fileExportReporter plugin", () => {
 		await fileExportReporter.onBrowserComplete(browser, result);
 
 		expect(fsWriteFileMock).toHaveBeenCalledTimes(3);
-		expect(fsWriteFileMock).toBeCalledWith("./karma-ui5-reports/filename", "content1");
-		expect(fsWriteFileMock).toBeCalledWith("./karma-ui5-reports/filename_1", "content2");
-		expect(fsWriteFileMock).toBeCalledWith("./karma-ui5-reports/filename_2", "content3");
+		expect(fsWriteFileMock).toBeCalledWith(expect.stringMatching(/filename$/), "content1");
+		expect(fsWriteFileMock).toBeCalledWith(expect.stringMatching(/filename_1$/), "content2");
+		expect(fsWriteFileMock).toBeCalledWith(expect.stringMatching(/filename_2$/), "content3");
+	});
+
+	it("onBrowserComplete - invalid export file path", async () => {
+		const result = {
+			exportFiles: [{
+				name: "../foo/filename",
+				content: "content1"
+			}]
+		};
+
+		pathJoinMock
+			.mockReturnValueOnce(resolvedTestPath)
+			.mockReturnValueOnce("/resolved/foo/filename")
+			.mockReturnValueOnce("/resolved/foo/filename");
+		fsAccessMock
+			.mockRejectedValueOnce({code: "ENOENT"});
+		fsWriteFileMock
+			.mockImplementationOnce(() => {});
+
+		const fileExportReporter = new FileExportReporter(base, config, logger);
+		await fileExportReporter.onBrowserComplete({}, result);
+
+		expect(fsWriteFileMock).toHaveBeenCalledTimes(0);
+		expect(log.warn).toBeCalledWith("Invalid export file path: /resolved/foo/filename\n\t" +
+			"Make sure the file path is in directory: " + resolvedTestPath);
+	});
+
+	it("onExit - done-function is only called if onBrowserComplete is processed (exitCode=0)", async () => {
+		const result = {
+			exportFiles: [{
+				name: "filename",
+				content: "content1"
+			}]
+		};
+		const doneFunction = jest.fn();
+
+		const fileExportReporter = new FileExportReporter(base, config, logger);
+
+		fileExportReporter.onExit(doneFunction);
+		expect(doneFunction).toHaveBeenCalledTimes(0); // done-function not yet called
+
+		fsAccessMock.mockRejectedValueOnce({code: "ENOENT"});
+		fsWriteFileMock.mockResolvedValueOnce();
+		await fileExportReporter.onBrowserComplete({}, result);
+
+		expect(doneFunction).toHaveBeenCalledTimes(1);
+		expect(doneFunction).toBeCalledWith(0);
+	});
+
+	it("onExit - done-function is only called if onBrowserComplete is processed (exitCode=1)", async () => {
+		const result = {
+			exportFiles: [{
+				name: "filename",
+				content: "content1"
+			}]
+		};
+		const doneFunction = jest.fn();
+
+		const fileExportReporter = new FileExportReporter(base, config, logger);
+
+		fileExportReporter.onExit(doneFunction);
+		expect(doneFunction).toHaveBeenCalledTimes(0); // done-function not yet called
+
+		pathJoinMock
+			.mockReturnValueOnce(resolvedTestPath)
+			.mockImplementationOnce(() => {
+				throw new Error("errorMsg");
+			});
+		fsAccessMock.mockRejectedValueOnce({code: "ENOENT"});
+		fsWriteFileMock.mockResolvedValueOnce();
+		await fileExportReporter.onBrowserComplete({}, result);
+
+		expect(log.warn).toBeCalledWith("An unexpected error occured while exporting files\n\terrorMsg");
+		expect(doneFunction).toHaveBeenCalledTimes(1);
+		expect(doneFunction).toBeCalledWith(1);
 	});
 });
